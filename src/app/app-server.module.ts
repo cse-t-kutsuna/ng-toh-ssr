f50001cb54c9aae12b789d271f837c0f7eb98625
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-12-08 05:56:27 +0900

    implement AppServerModule

--- src/app/app-server.module.ts
+++ src/app/app-server.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServerModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AppModule,
+    ServerModule,
+    ModuleMapLoaderModule,
   ],
-  declarations: []
+  declarations: [],
+  bootstrap: [AppComponent],
 })
 export class AppServerModule { }
