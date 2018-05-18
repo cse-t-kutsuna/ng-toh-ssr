06a82c14d5a2afbcd9a57104b35c4d55bd04e335
Teruyoshi Kutsuna <cse_t_kutsuna@incrementp.co.jp>
2018-05-18 10:36:24 +0900

    feat(server/index.ts): add heroes api, replace express to koa.

--- server/index.ts
+++ server/index.ts
@@ -3,38 +3,80 @@ import 'reflect-metadata';
 
 import {enableProdMode} from '@angular/core';
 
-import * as express from 'express';
+// import * as express from 'express';
+import * as Koa from 'koa';
+import * as route from 'koa-route';
+import * as serve from 'koa-static';
+import * as accesslog from 'koa-accesslog';
+
 import {join} from 'path';
+
+/* Temporary fix: using non blocking ngFor on server
+import {NgForOf} from "@angular/common";
+import {NgForOfNb} from "../src/app/server-common/src/directives";
+
+(<any>NgForOf).prototype._applyChanges = NgForOfNb.prototype._applyChanges;
+*/
+
+const blocked = require('blocked-at');
+blocked((time, stack) => {
+  console.log(`Blocked for ${time}ms, operation started here:`, stack)
+});
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
 // Express server
-export const app = express();
+// export const app = express();
+export const app = new Koa();
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('../dist/serverApp/main.bundle');
 
 // Express Engine
-import {ngExpressEngine} from '@nguniversal/express-engine';
+// import {ngExpressEngine} from '@nguniversal/express-engine';
 // Import module map for lazy loading
 import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+import {renderModuleFactory} from "@angular/platform-server";
+import {readFileSync} from "fs";
 
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
 
-  providers: [
-    provideModuleMap(LAZY_MODULE_MAP)
-  ],
+let heroes = [];
+for (let i = 0; i < 5000; i++) {
+  heroes.push({id: i, name: `Hero-${i}`});
+}
+
+// app.use(morgan('dev'));
+app.use(accesslog());
+
+// Our index.html we'll use as our template
+const template = readFileSync(join(process.cwd(), 'dist', 'browserApp', 'index.html')).toString();
+
+app.use(route.get('/api/heroes', (ctx) => {
+  ctx.body = heroes;
 }));
 
-app.set('view engine', 'html');
-app.set('views', join(process.cwd(), 'dist', 'browserApp'));
+app.use(route.get('/api/heroes/:id', (ctx, id) => {
+  ctx.body = heroes.find((hero) => hero.id === +id);
+}));
+
+// app.set('view engine', 'html');
+// app.set('views', join(process.cwd(), 'dist', 'browserApp'));
 
 // Server static files from /browser
-app.get('*.*', express.static(join(process.cwd(), 'dist', 'browserApp')));
+app.use(route.get('*.*', serve(join(process.cwd(), 'dist', 'browserApp'))));
 
 // All regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render(join(process.cwd(), 'dist', 'browserApp', 'index.html'), {req});
-});
+app.use(route.get('(.*)', (ctx) => {
+  return renderModuleFactory(AppServerModuleNgFactory, {
+    // Our index.html
+    document: template,
+    url: ctx.url,
+    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ]
+  }).then(html => {
+    ctx.body = html;
+  });
+}));
